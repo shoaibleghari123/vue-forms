690a9eea78e73cddc2d2d1724851c369f1dc639b
muhammad_Shoaib <muhammad_Shoaib@pitb.gov.pk>
2024-04-26 23:35:53 +0500

    initial commit

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,25 @@
+<template>
+  <input
+          type="radio"
+          :checked="modelValue===value"
+          :value="value"
+          @change="$emit('update:modelValue', value)"
+          v-bind="$attrs"
+        />
+        <label v-if="label">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: [String, Number],
+      required: true
+    }
+  }
+}
+</script>
